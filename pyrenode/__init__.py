70640d249cdb85c30bd19cd677bce198b011a454
Piotr Zierhoffer <pzierhoffer@antmicro.com>
2022-05-19 11:26:55 +0200

    Major refactor

--- pyrenode/__init__.py
+++ pyrenode/__init__.py
@@ -1,95 +1,109 @@
-import re, telnetlib
+import re
+import telnetlib
+import sys
 import pexpect
-from typing import Tuple
+import psutil
+import robot.libraries.Remote
+from dataclasses import dataclass
 
 logfile = '/tmp/renode_log.txt'
-global renode_connection
-renode_connection = None
-global renode_log
-renode_log = None
-global port
-global telnet
-global renode_connected
-global keywords_initialized
-renode_connected = False
-keywords_initialized = False
 
-import logging
-LOGGER = logging.getLogger(__name__)
+class State:
+    def __init__(self):
+        self.telnet_port = 0
+        self.robot_port = 0
+        self.renode_connected = False
+        self.keywords_initialized = False
+        self.log_expect = None
+        self.renode_connection = None
+
+    def clean(self):
+        if self.renode_connection is not None:
+            tell_renode('q')
+            self.renode_connection.close()
+
+        if self.log_expect is not None:
+            try:
+                psutil.Process(self.log_expect.pid)
+            except psutil.NoSuchProcess:
+                pass
+
+        self.renode_connected = False
+        self.keywords_initialized = False
+        self.log_expect = None
+        self.renode_connection = None
+
+
+global state
+state = State()
+
 
 def escape_ansi(line):
     ansi_escape = re.compile(r'(?:\x1B[@-_]|[\x80-\x9F])[0-?]*[ -/]*[@-~]')
     return ansi_escape.sub('', line)
 
-def connect_renode(spawn_renode=True, telnet_port=4567, robot_port=3456):
 
-    global renode_connected
-    if renode_connected:
+def connect_renode(spawn_renode=True, telnet_port=4567, robot_port=3456):
+    if state.renode_connected:
         print("Renode already connected...")
         return
-    global port
-    global telnet
-    telnet = telnet_port
-    port = robot_port
-    global renode_log
+
+    state.telnet_port = telnet_port
+    state.robot_port = robot_port
+
     if spawn_renode:
         command = f"--plain --port {str(telnet_port)} --robot-server-port {str(robot_port)} --disable-xwt"
         try:
-            renode_log = pexpect.spawn(f'renode {command}')
+            state.log_expect = pexpect.spawn(f'renode {command}')
         except pexpect.ExceptionPexpect:
-            renode_log = pexpect.spawn(f'renode-run exec -- {command}')
+            state.log_expect = pexpect.spawn(f'renode-run exec -- {command}')
 
-        renode_log.stripcr = True
+        state.log_expect.stripcr = True
         assert expect_log(f"Monitor available in telnet mode on port {str(telnet_port)}").match is not None
         assert expect_log(f"Robot Framework remote server is listening on port {str(robot_port)}").match is not None
 
-    global renode_connection
     if telnet_port is not None:
-        renode_connection = telnetlib.Telnet("localhost", telnet_port)
+        state.renode_connection = telnetlib.Telnet("localhost", telnet_port)
         assert expect_cli("(monitor)").match is not None
         # first char gets eaten for some reason, hence the space
         tell_renode(" ")
         tell_renode(f"logFile @{logfile}")
-    #tell_renode("Clear") # this totally breaks stuff - why?
-    renode_connected = True
+
+    state.renode_connected = True
+
 
 def shutdown_renode():
+    state.clean()
 
-    import psutil
 
-    for proc in psutil.process_iter():
-        if "renode" in proc.name().casefold():
-            proc.kill()
-
-    if renode_connection:
-        renode_connection.close()
-
+# this should use EITHER telnet or ExecuteCommand keyword, whichever is available
 def tell_renode(string, newline = True):
     if newline:
         string += '\n'
-    renode_connection.write(string.encode())
+    state.renode_connection.write(string.encode())
 
-from dataclasses import dataclass
 
 @dataclass
 class Result:
     text: str = ''
     match: object = None
 
+
 def read_until(string, timeout = 1):
-    return escape_ansi(renode_connection.read_until(string.encode(), timeout).decode())
+    return escape_ansi(state.renode_connection.read_until(string.encode(), timeout).decode())
+
 
 def expect_cli(string, timeout = 15):
     # take into consideration that the outpit will include some CR chars
     expected = re.escape(string).replace('\n','\r*\n\r*')
-    idx, matches, data = renode_connection.expect([expected.encode()], timeout)
+    idx, matches, data = state.renode_connection.expect([expected.encode()], timeout)
     return Result(escape_ansi(data.decode()), matches)
 
-def expect_log(regex, timeout = 15):
-    result = renode_log.expect(regex, timeout=timeout)
-    return Result(escape_ansi(renode_log.before.decode()), renode_log.match)
 
-from robot.libraries.BuiltIn import BuiltIn
+def expect_log(regex, timeout = 15):
+    result = state.log_expect.expect(regex, timeout=timeout)
+    return Result(escape_ansi(state.log_expect.before.decode()), state.log_expect.match)
+
 
 def bind_function(remote, name):
     def func(*args, **kwargs):
@@ -100,23 +114,19 @@ def bind_function(remote, name):
         return message
     return func
 
-import sys
 
 def get_keywords():
-
-    global keywords_initialized
-    if keywords_initialized:
+    if state.keywords_initialized:
         print("Keywords already initialized...")
         return
+
     current_module = sys.modules['__main__']
 
-    r = BuiltIn()
-    import robot.libraries.Remote
-    remote = robot.libraries.Remote.Remote(uri="http://0.0.0.0:"+str(port))
+    remote = robot.libraries.Remote.Remote(uri="http://0.0.0.0:" + str(state.robot_port))
     keywords = remote.get_keyword_names()
     print(f"Importing keywords: {', '.join(keywords)}")
     print()
     for k in keywords:
         func = bind_function(remote, k)
         setattr(current_module, k, func)
-    keywords_initialized = True
+    state.keywords_initialized = True
