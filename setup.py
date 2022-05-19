2f9222b6ccb0e35fd243b5199d22b26fcd31a210
Piotr Zierhoffer <pzierhoffer@antmicro.com>
2022-05-19 11:26:31 +0200

    Add robotframework dependency

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(name='pyrenode',
-      version='0.1',
+      version='0.2',
       description='Very basic Python lib to talk to Renode',
       author='Antmicro',
       author_email='mgielda@antmicro.com',
       install_requires=[
-          'pexpect', 'dataclasses', 'psutil'
+          'pexpect', 'dataclasses', 'psutil', 'robotframework==4.0.1'
       ],
       license='MIT',
       packages=['pyrenode'])
