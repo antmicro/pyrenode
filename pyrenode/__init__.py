344740f85ce96a6c5047ce34f848f29c3053f81e
Patryk Koscik <pkoscik@antmicro.com>
2023-03-10 15:25:59 +0100

    Rework initialization wait system
    This patch:
    - Extracts common waiting logic to `init_wait` fnc
    - Adds service-specific `try_get_*` fncs
    - Reworks process cleanup in destructor

--- pyrenode/__init__.py
+++ pyrenode/__init__.py
@@ -5,6 +5,9 @@ import robot.libraries.Remote
 import sys
 import telnetlib
 import time
+import tempfile
+import os
+import signal
 from dataclasses import dataclass
 
 logfile = '/tmp/renode_log.txt'
@@ -15,7 +18,8 @@ class State:
         self.robot_port = 0
         self.renode_connected = False
         self.keywords_initialized = False
-        self.subprocess = None
+        self.subprocess = None    # Might point to renode-run
+        self.renode_pid = None    # Always points to renode
         self.renode_connection = None
 
     def __del__(self):
@@ -26,10 +30,14 @@ class State:
             tell_renode('q')
             self.renode_connection.close()
 
-        if self.subprocess is not None:
+        pids = [self.subprocess.pid, self.renode_pid]
+        for pid in pids:
             try:
-                psutil.Process(self.subprocess.pid)
-            except psutil.NoSuchProcess:
+                os.kill(pid, signal.SIGTERM)
+            except ProcessLookupError:
+                pass
+            except Exception as e:
+                print(f"An exception occured: {e}\nA zombie proces might be left behind!")
                 pass
 
         self.renode_connected = False
@@ -47,6 +55,70 @@ def escape_ansi(line):
     return ansi_escape.sub('', line)
 
 
+def try_get_renode(state):
+    parent = psutil.Process(state.subprocess.pid)
+    children = parent.children(recursive=False)
+
+    # Will throw an exception and end the function execution, if no 'renode' process is found
+    renode_process = next(ps for ps in children if ps.name() == 'renode')
+
+    # Set renode_pid when successful
+    state.renode_pid = renode_process.pid
+
+
+def try_get_robot(state):
+    renode_port_file = os.path.join(tempfile.gettempdir(), f'renode-{state.renode_pid}', 'robot_port')
+
+    # Will throw an exception and end the function execution, if no file is found...
+    with open(renode_port_file, 'r') as f:
+        port_num_str = f.readline()
+        if port_num_str == '':
+            # or if the file is empty
+            raise ValueError
+
+        # Set robot_port when succesful
+        state.robot_port = int(port_num_str)
+
+
+def try_get_telnet(state):
+    state.renode_connection = telnetlib.Telnet("localhost", state.telnet_port)
+
+    # Will throw an exception and end the function execution, if the telnet was not connected
+    assert expect_cli("(monitor)").match is not None
+
+
+def init_wait(state, function, timeout, retry_time, fail_msg=None):
+    '''
+        This method is a wrapper to the try_get_* functions.
+
+        The try_get_* functions indicate a failure with the intention
+        to retry by throwing any exception. The exception is ignored
+        for attempt-times. The attempt+1 exception will be raised, and an
+        optional error message will be printed out.
+
+        timeout: total time in seconds to keep retrying the 'function'
+                 before giving up and raising an exception.
+
+        retry_time: time in seconds to wait between each retry attempt of the 'function'.
+
+        These parameters are used to calculate the number of attempts to execute the 'function'.
+    '''
+    completed = False
+    attempts = int(timeout/retry_time)
+    while not completed:
+        try:
+            # The 'function' must throw an exception upon failure
+            function(state)
+            completed = True
+        except:
+            if attempts <= 0:
+                if fail_msg is not None:
+                    print(fail_msg)
+                raise
+            attempts -= 1
+            time.sleep(retry_time)
+
+
 def connect_renode(spawn_renode=True, telnet_port=4567, robot_port=0, timeout=10, retry_time=0.2):
     if state.renode_connected:
         print("Renode already connected...")
@@ -54,30 +126,31 @@ def connect_renode(spawn_renode=True, telnet_port=4567, robot_port=0, timeout=10
 
     state.telnet_port = telnet_port
     state.robot_port = robot_port
+    wait_for_renode = False
+    robot_connected = False
+    telnet_connected = False
 
     if spawn_renode:
         command = ['--plain', '--port', str(telnet_port), '--robot-server-port', str(robot_port), '--disable-xwt']
 
         try:
             state.subprocess = subprocess.Popen(['renode', *command], stdout=subprocess.DEVNULL)
+            wait_for_renode = False
+            state.renode_pid = state.subprocess.pid
         except OSError:
             state.subprocess = subprocess.Popen(['renode-run', 'exec', '--', *command], stdout=subprocess.DEVNULL)
+            wait_for_renode = True
 
-    # Waiting for Renode to start the Telnet service
-    # Defaults 10/0.2 -> maximum of about 50 connection attempts
+    # Wait for Renode process if running from 'renode-run'
+    if wait_for_renode:
+        init_wait(state, try_get_renode, timeout, retry_time, fail_msg="pyrenode could not start Renode.")
+
+    # Wait for Robot server to start
+    init_wait(state, try_get_robot, timeout, retry_time, fail_msg="pyrenode was not able to detect Robot server port.")
+
+    # Wait for Renode to start the Telnet service
     if telnet_port is not None:
-        done = False
-        tries = int(timeout/retry_time)
-        while not done:
-            try:
-                state.renode_connection = telnetlib.Telnet("localhost", telnet_port)
-                assert expect_cli("(monitor)").match is not None
-                done = True
-            except:
-                if tries <= 0:
-                    raise
-                tries -= 1
-                time.sleep(retry_time)
+        init_wait(state, try_get_telnet, timeout, retry_time, fail_msg="pyrenode was not able to connect to the Telnet port.")
 
         # first char gets eaten for some reason, hence the space
         tell_renode(" ")
